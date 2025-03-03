ed9a2c3b94fe8db7ecc4dd0eb8f5fda8c9ead75c
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-03-03 21:37:10 +0000

    feat: correctly cipher uppercase characters
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
-# @param str [String]
-# @param shift [Integer]
-# @return [String]
-def ceasar_cipher(str, shift)
-  alpha = ('a'..'z').to_a + ('A'..'Z').to_a
-  str.split('').map do |c|
-    if c =~ /\A[[:alpha:]]+\z/
-      alpha_idx = alpha.find_index(c)
-      if alpha[alpha_idx + shift].nil?
-        alpha.reverse[alpha_idx + shift]
-      else
-        alpha[alpha_idx + shift]
-      end
-    else
-      c
-    end
-  end.join
+# @param str [String] input string to be encrypted
+# @param shift [Integer] number of positions to shift characters
+# @return [String] encrypted string
+def caesar_cipher(str, shift)
+  shift %= 26
+  str.chars.map { |char| shift_char(char, shift) }.join
 end
 
-pp ceasar_cipher('What a string!', 5)
+# @param char [String] character to shift
+# @param shift [Integer] number of positions to shift by
+# @return [String] shifted character
+def shift_char(char, shift)
+  return char unless char =~ /[A-Za-z]/
+
+  base = char =~ /[a-z]/ ? 'a'.ord : 'A'.ord
+  ((char.ord - base + shift) % 26 + base).chr
+end
+
+pp caesar_cipher('What a string!', 5)
+pp caesar_cipher('Hello, World!', 3)
