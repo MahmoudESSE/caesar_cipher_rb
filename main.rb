fb9a0bced26935494dcac0726c85414993af7143
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-03-03 21:21:01 +0000

    feat: A beginning algorithm
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- main.rb
+++ main.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: false
 
-puts 'Hello, World!'
+# @param str [String]
+# @param shift [Integer]
+# @return [String]
+def ceasar_cipher(str, shift)
+  alpha = ('a'..'z').to_a + ('A'..'Z').to_a
+  str.split('').map do |c|
+    if c =~ /\A[[:alpha:]]+\z/
+      alpha_idx = alpha.find_index(c)
+      if alpha[alpha_idx + shift].nil?
+        alpha.reverse[alpha_idx + shift]
+      else
+        alpha[alpha_idx + shift]
+      end
+    else
+      c
+    end
+  end.join
+end
+
+pp ceasar_cipher('What a string!', 5)
